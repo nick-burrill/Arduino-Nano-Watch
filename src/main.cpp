a35323f96eab67abb9b41d0f8a05a911c20021e2
nick-burrill <76707213+nick-burrill@users.noreply.github.com>
2021-01-03 12:00:21 -0500

    Updated Comment

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 // The pins for I2C are defined by the Wire-library. 
-// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino UNO/NANO:       A4(SDA), A5(SCL)
 #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
